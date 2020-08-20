55cdbd5ffce4e9747f202c03c5972cd386b48551
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-20 16:27:41 +0900

    created all the component files and imported react boilerplate in them

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
 const Hello = ({ name }) => {
   return (
     <div>
-      Hello,
-      {name}
+      Hello, {name}
     </div>
   );
 };
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name="Bob" />, root);
 }
