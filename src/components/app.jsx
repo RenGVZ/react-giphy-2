047a3bf895e7f513d9516844dddcf8a95a90750d
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-20 18:11:05 +0900

    giphy practice

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,46 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+import giphy from 'giphy-api';
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "fqtxCwUd2t6dCzILMq"
+    };
+  }
+
+  search = (query) => {
+    giphy('RWKxeBAYUzwNsEWxmRDRr8plikoy1sJm').search({
+      q: query,
+      rating: 'g',
+      limit: 15
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+    );
+  }
 }
 
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,7 @@ import ReactDOM from 'react-dom';
 import '../assets/stylesheets/application.scss';
 import App from './components/app';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello, {name}
-    </div>
-  );
-};
-
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Bob" />, root);
+  ReactDOM.render(<App />, root);
 }
